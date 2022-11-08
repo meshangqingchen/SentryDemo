1f59945ed0b851b520df4efc8b201ade8bad6707
lichangcheng <lichangcheng@xiaomi.com>
2022-11-08 20:35:37 +0800

    no message

--- App.js
+++ App.js
@@ -7,15 +7,14 @@
  */
 
 import React, { Component } from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -28,9 +27,6 @@ import {
 
 import * as Sentry from '@sentry/react-native';
 
-Sentry.init({ 
-  dsn: 'https://c815a4494fbc4e9c845509f94fc5bf72@o4504098518925312.ingest.sentry.io/4504098529083392', 
-});
 class Section extends Component {
   constructor(props) {
     super(props);
@@ -38,6 +34,79 @@ class Section extends Component {
 
   componentDidMount() {
     // super.componentDidMount();
+    Sentry.init({ 
+      dsn: 'https://c815a4494fbc4e9c845509f94fc5bf72@o4504098518925312.ingest.sentry.io/4504098529083392', 
+      beforeSend(event) {
+        console.log('lcc_____eventeventeventeventeventevent', event);
+        return event;
+      },
+      // enableAppStartTracking: true,
+      tracesSampler(samplingContext) {
+        console.log('lcc_____samplingContext = ', samplingContext);
+        return 1.0;
+      }
+    });
+  }
+
+
+  validateShoppingCartOnServer () {
+    return {
+      code : 0,
+      list : ['1', '2', 'lcc']
+    }
+  }
+
+  processAndValidateShoppingCart () {
+    
+  }
+
+  test() {
+    // console.log('lcc');
+    // throw new Error("34567890- 2222222222222");
+    
+    const transaction = Sentry.startTransaction({ name: "lcc_ceshi_分步骤" });
+    Sentry.getCurrentHub().configureScope(scope => scope.setSpan(transaction));
+    const result = this.validateShoppingCartOnServer();
+
+    setTimeout(() => {
+      const span = transaction.startChild({
+        data: {
+          result,
+        },
+        op: 'Lcc_task1',
+        description: `Lcc_步骤1`,
+      });
+      this.processAndValidateShoppingCart(result);
+      span.setStatus('ok');
+      span.finish();
+
+      setTimeout(() => {
+        const span1 = transaction.startChild({
+          data: {
+            result,
+          },
+          op: 'Lcc_task2',
+          description: `Lcc_步骤2`,
+        });
+        this.processAndValidateShoppingCart(result);
+        span1.setStatus('ok');
+        span1.finish();
+        setTimeout(() => {
+          const span3 = transaction.startChild({
+            data: {
+              result,
+            },
+            op: 'Lcc_task3',
+            description: `Lcc_步骤3`,
+          });
+          this.processAndValidateShoppingCart(result);
+          span3.setStatus('ok');
+          span3.finish();
+          
+          transaction.finish();
+        }, 500);
+      }, 1000);
+    }, 2000);
   }
 
   render() {
@@ -45,15 +114,19 @@ class Section extends Component {
     const {title, children} = this.props;
     return (
       <View style={styles.sectionContainer}>
-        <Text
-          style={[
-            styles.sectionTitle,
-            {
-              color: isDarkMode ? Colors.white : Colors.black,
-            },
-          ]}>
-          {title}
-        </Text>
+        <TouchableOpacity onPress={()=>{
+          this.test();
+        }}>
+          <Text
+            style={[
+              styles.sectionTitle,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>
+            {title}
+          </Text>
+        </TouchableOpacity>
         <Text
           style={[
             styles.sectionDescription,
@@ -136,5 +209,4 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
-
-export default App;
+export default Sentry.wrap(App);
